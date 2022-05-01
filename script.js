f92def18b43753c64030d06bd35ad8a1b8c26e19
Max <bobandjoe132@gmail.com>
2022-05-01 00:31:29 -0700

    proggers

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 var progress = 0;
+
+
 function prog() {
     document.getElementById("pb").style.width = progress + "%";
-    progress++;
+    progress += 1;
+    if (progress > 50) {
+        document.getElementById("pbp").innerHTML = "I mean, what?";
+    } else {
+        document.getElementById("pbp").innerHTML = "Downloading virus...";
+    }
     if (progress > 100) {
         progress = 0;
     }
 }
+
+function progCont() {
+    var interval = setInterval(prog, 100);
+}
+
+progCont();
+
+
+
